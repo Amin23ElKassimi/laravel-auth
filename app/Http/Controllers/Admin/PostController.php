f86de627c99b98c584e856ec0356573756a8c819
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-02-12 19:51:41 +0100

    Controller Store Fillable in Mode;

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -25,6 +25,7 @@ class PostController extends Controller
     public function create()
     {
         //
+        return view('admin.posts.create');
     }
 
     /**
@@ -33,14 +34,24 @@ class PostController extends Controller
     public function store(Request $request)
     {
         //
+        // dd($request->all());
+        $data = $request->validate($this->rules);
+
+
+        $post = Post::create($data);
+
+        return redirect()->route('admin.posts.show', $post);
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Post $post)
     {
         //
+        return view('admin.posts.show', compact('post'));
+
     }
 
     /**
